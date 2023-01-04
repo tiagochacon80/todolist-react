558f425a085c0274a9b465ff72b47debef9981a7
Tiago Chacon <tiagochacon80@gmail.com>
2023-01-04 10:05:44 -0500

    creating header

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import * as C from './App.styles';
 const App = () => {
   return (
     <C.Container>
-      ...
+      <C.Area>
+        <C.Header>To do list</C.Header>
+      </C.Area>
     </C.Container>
   );
 }
