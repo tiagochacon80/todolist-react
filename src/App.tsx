5ee5c16c2744e858875f7ff792facb947ad76c9a
Tiago Chacon <tiagochacon80@gmail.com>
2023-01-04 14:06:04 -0500

    finalizing the item list and restarting at the end

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,44 @@ import { Item } from './types/Item';
 import { ListItem } from './components/ListItem';
 import { AddArea } from './components/AddArea';
 
+
 const App = () => {
-const [list, setList] = useState<Item[]>([
-  { id: 1, name: 'Christmas shopping', done: false },
-  { id: 2, name: 'Buy ingredients for cake recipe', done: true },
+const [list, setList] = useState<Item[]>([  
 ]);
+
+const handleAddTask = (taskName: string) => {
+  let newList = [...list];
+  newList.push({
+    id: list.length + 1,
+    name: taskName,
+    done: false
+  });
+  setList(newList);
+}
+
+const handleTaskChange = (id: number, done: boolean) => {
+  let newList = [...list];
+  for(let i in newList) {
+    if(newList[i].id === id) {
+      newList[i].done = done;
+    }
+  }
+  setList(newList);
+}
 
   return (
     <C.Container>
       <C.Area>
         <C.Header>To do list</C.Header>  
 
-        <AddArea />      
+        <AddArea onEnter={handleAddTask} />      
 
         {list.map((item, index)=>(
-          <ListItem key={index} item={item} />
+          <ListItem
+           key={index}
+           item={item}
+           onChange={handleTaskChange}
+            />
         ))}
 
       </C.Area>
