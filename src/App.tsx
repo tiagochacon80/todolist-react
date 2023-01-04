4b4e4ea69cd74298da83fb70026659b13527c5b0
Tiago Chacon <tiagochacon80@gmail.com>
2023-01-04 11:01:50 -0500

    adding to the item list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem';
 
 const App = () => {
+const [list, setList] = useState<Item[]>([
+  { id: 1, name: 'Christmas shopping', done: false },
+  { id: 2, name: 'Buy ingredients for cake recipe', done: false },
+]);
+
   return (
     <C.Container>
       <C.Area>
-        <C.Header>To do list</C.Header>
+        <C.Header>To do list</C.Header>        
+
+        {list.map((item, index)=>(
+          <ListItem />
+        ))}
+
       </C.Area>
     </C.Container>
   );
---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,9 @@
+import * as C from './styles';
+
+export const ListItem = () => {
+    return (
+        <C.Container>
+            Hello world...
+        </C.Container>
+    );
+}
